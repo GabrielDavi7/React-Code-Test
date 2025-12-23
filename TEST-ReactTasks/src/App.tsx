943458d3fa02f78c17674c32b48df41912433e37
GabrielDavi7 <gabrielbaiamota765@gmail.com>
2025-12-22 22:47:00 -0300

    ADD: Testes iniciais

--- TEST-ReactTasks/src/App.tsx
+++ TEST-ReactTasks/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Tasks from "./components/Tasks";
+import AddTasks from "./components/AddTasks";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  //==================== teste do app realizados aqui ====================
+  /* testes states
+  const [TesteState, setTesteState] = useState("teste");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>{TesteState}</h1>;
+      <button
+        onClick={() => {
+          setTesteState("Mensagem Modificada");
+        }}
+      >
+        Mudar mensagem
+      </button>
+    </div>
+  );
+  Fim teste states*/
 
-export default App
+  /* testes tailwind
+
+    return (
+    <div>
+      <h1 className="text-red-800">Task Manager</h1>
+      <AddTasks />
+      <Tasks />
+    </div>
+  );
+    Fim teste tailwind*/
+
+  //==================== Fim do teste do app aqui ====================
+  return (
+    <div>
+      <AddTasks />
+    </div>
+  );
+}
+export default App;
---
+++ TEST-ReactTasks/src/components/AddTasks.tsx
@@ -0,0 +1,4 @@
+function AddTasks() {
+  return <h1>AddTasks</h1>;
+}
+export default AddTasks;
